b589540fb0a7d71d45de075b2e41f2b5197252c2
jeetbehera <jeet.behera@probiteducation.com>
2025-12-11 13:24:21 +0530

    pagination changes

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -60,7 +60,6 @@ class ExpenseController extends Controller
 
         // Get paginated results
         $expenses = $query->paginate(10);
-        dd($expenses);
         
         // Get totals
         $totalExpenses = Expense::where('user_id', $userId)->sum('amount');
